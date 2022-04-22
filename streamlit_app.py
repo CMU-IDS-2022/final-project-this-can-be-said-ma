6b05f6d7a1ccf5cb1eb7c2ad16e0952edf6d8e6d
Jellyhurricane <wenxing@JellyhuanedeMBP.fios-router.home>
2022-04-22 16:22:40 -0400

    mofidy part 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,45 @@ import altair as alt
 # import plotly.express as px
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
+
+@st.cache  # add caching so we load the data only once
+def load_data():
+    mental_df = pd.read_csv("data/Mental Health Checker.csv", encoding = "ISO-8859-1")
+    mental_df = mental_df[['gender', 'age', 'marital', 'income', 'loan', 'social_media', 'sleep_disorder', 'mental_disorder', 'therapy']]
+    return mental_df
+
+
+def plot(title, df, xlabel, ylabel, column, index):
+    # plt.clf()
+    dfs = []
+    for i in range(len(index)):
+      dfs.append(df[df[column] == index[i]])
+    P, D, S, A, N = [0] * len(index), [0] * len(index), [0] * len(index), [0] * len(index), [0] * len(index)
+    disorder = ['Panic attack', 'depression', 'stress', 'anxiety']
+    for i in range(len(index)):
+      P[i] = len(dfs[i][dfs[i]["mental_disorder"] == disorder[0]])
+      D[i] = len(dfs[i][dfs[i]["mental_disorder"] == disorder[1]])
+      S[i] = len(dfs[i][dfs[i]["mental_disorder"] == disorder[2]])
+      A[i] = len(dfs[i][dfs[i]["mental_disorder"] == disorder[3]])
+        # N[i] = len(df[df[column] == index[i]]["mental_disorder"] == disorder[i])
+    for i in range(len(index)):
+      N[i] = len(df[df[column] == index[i]]) - P[i] - D[i] - S[i] - A[i]
+    plotdata = pd.DataFrame({
+    "Panic attack":P,
+    "Depression":D,
+    "Stress":S,
+    "Anxiety":A,
+    "N/A":N
+    }, 
+    index=index
+)
+    plotdata.plot(kind="bar")
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(xlabel)
+
+
 
 st.title("Stress Analysis: Narrative of stress that enhances people’s understanding of it")
 
@@ -21,8 +60,25 @@ image = Image.open('img/1.png')
 st.image(image)
 
 st.markdown("First, let's explore whether stress level has specific relationships with gender, \
-age, marital status, income level, loan, time spent in social media a day or sleep disorder. Please select the \
-factors you are interested in and analyze the bar charts.")
+age, marital status, income level, loan, time spent in social media a day or sleep disorder. ")
+
+mental_df = load_data()
+factor = st.selectbox("Please select the factors you are interested in and analyze the bar charts.", ["gender", "age", "marital", "income", "loan", "social_media", "Mental disorder type"])
+if factor == "gender":
+   plot("Mental disorder distribution among different genders", mental_df, "Mental disorder type", "Number of interviewees", factor, ['female', 'male'])
+elif factor == "age":
+   plot("Mental disorder distribution among different age groups", mental_df, "Mental disorder type", "Number of interviewees", factor, ['13-19', '20-26', '27-33', '34-44', '45 or more'])
+elif factor == "marital":
+   plot("Mental disorder distribution among different  marital status groups", mental_df, "Mental disorder type", "Number of interviewees", factor, ['single', 'marital', 'divorced', 'separated'])
+elif factor ==  "income":
+   plot("Mental disorder distribution among different income level groups", mental_df, "Mental disorder type", "Number of interviewees", factor, ['<10', '<20', '<30', '30+', '50+'])
+elif factor ==  "loan":
+   plot("Relationship between mental disorder and loan", mental_df, "Mental disorder type", "Number of interviewees", factor, ['yes', 'no'])
+elif factor ==  "social_media":
+   plot("Mental disorder distribution with time spent on social media per day", mental_df, "Mental disorder type", "Number of interviewees", factor, ['<1 hour', '<2 hours', '<3 hours', '3+ hours'])
+elif factor ==  "Mental disorder type":
+   plot("Relationship between mental disorder and sleep disorder", mental_df, "Mental disorder type", "Number of interviewees", factor, ['yes', 'no'])
+
 
 st.sidebar.title("待填入Stress Data Analysis: To have an in-depth understanding of the following questions")
 st.sidebar.markdown("待填入This application is a Streamlit dashboard to enhances people’s understanding of stress")
